b2d6eddb120ab9058557f9976b1dc98655e73060
JFL110 <typus24@hotmail.com>
2020-09-14 19:18:00 +0100

    eslint ignores

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
-module.exports = { "env": { 
-"browser": true, 
-"es2020": true,
-"jest": true
- }, "extends": [ "eslint:recommended", "plugin:react/recommended" ], "parserOptions": { "ecmaFeatures": { "jsx": true }, "ecmaVersion": 11, "sourceType": "module" }, "plugins": [ "react" ], "rules": {
- "react/prop-types": 0,
- "react/display-name": 0 ,
-"react/react-in-jsx-scope" : 0
-}, "settings": { "react": { "version": "detect" } } };
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true,
+        "jest": true
+    }, "extends": ["eslint:recommended", "plugin:react/recommended"], "parserOptions": { "ecmaFeatures": { "jsx": true }, "ecmaVersion": 11, "sourceType": "module" }, "plugins": ["react"], "rules": {
+        "react/prop-types": 0,
+        "react/display-name": 0,
+        "react/react-in-jsx-scope": 0,
+        "react/no-unescaped-entities": 0
+    }, "settings": { "react": { "version": "detect" } }
+};
